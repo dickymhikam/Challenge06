421ac59b56f2d7522600b596e4d557fb705450d8
dickymhikam <dickymhikam321@gmail.com>
2023-11-03 17:05:42 +0700

    last final

--- src/components/GoogleLogin.js
+++ src/components/GoogleLogin.js
@@ -7,42 +7,7 @@ import { useNavigate } from "react-router-dom";
 import { registerLoginWithGoogle } from "../redux/actions/authActions";
 
 function GoogleLogin({ buttonText }) {
-//   const registerLoginWithGoogleAction = async (accessToken) => {
-//     try {
-//       let data = JSON.stringify({
-//         access_token: accessToken,
-//       });
-      
-// let config = {
-//     method: "post",
-//     maxBodyLength: Infinity,
-//     url: 'https://shy-cloud-3319.fly.dev/api/v1/auth/google',  
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     data: data,
-//   };
-  
-//       const response = await axios.request(config);
-//       const { token } = response.data.data;
 
-//       localStorage.setItem("token", token);
-
-  
-//       window.location.href = "/";
-//     } catch (error) {
-//       if (axios.isAxiosError(error)) {
-//         toast.error(error.response.data.message);
-//         return;
-//       }
-//       toast.error(error.message);
-//     }
-//   };
-
-//   const loginWithGoogle = useGoogleLogin({
-//     onSuccess: (responseGoogle) =>
-//       registerLoginWithGoogleAction(responseGoogle.access_token),
-//   });
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -57,9 +22,5 @@ function GoogleLogin({ buttonText }) {
     </Button>
   );
 }
-
-// GoogleLogin.propTypes = {
-//   buttonText: PropTypes.string.isRequired,
-// };
 
 export default GoogleLogin;
